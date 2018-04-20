49a8a4a5a3e43db72626f6f2aa61521958c49b2f
Seth_PC <sethamin.7@gmail.com>
2018-04-20 11:36:19 -0700

    Finished part 2 section 5

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -35,10 +35,29 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 # Building the optimal model using Backward Elimination
+# X = sm.add_constant(X)
+# X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# regressor_OLS.summary()
+# X_opt = X[:, [0, 3, 5]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# print(regressor_OLS.summary())
+
+
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(endog=y, exog=x).fit()
+        pvalue_list = regressor_OLS.pvalues
+        pvalue_i = np.argmax(pvalue_list)
+        pvalue_max = pvalue_list[pvalue_i]
+        if pvalue_max > sl:
+            x = np.delete(x, pvalue_i, 1)
+        else:
+            break
+    return x
+
+
 X = sm.add_constant(X)
-X_opt = X[:, [0, 1, 2, 3, 4, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-regressor_OLS.summary()
-X_opt = X[:, [0, 3, 5]]
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
+sol = backwardElimination(X, 0.05)
+print(sol)
